168818a30060d4f7427102307a9b837db22f9e79
Leandro Nascimento <leandrolnc@hotmail.com>
2018-06-28 00:42:54 -0300

    components part 2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
   render() {
     return (
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     );
   }
 }
